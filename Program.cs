36fd3ad54636d498053c7b8d2d5361c650eb9142
Ashlyn Wright <362581399@GoTVDSB.ca>
2025-10-29 09:03:59 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace topic_7._1___While_loop_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ashlyn Wright
+            //top7.1 While loop assignment
+
+
+
+
+
+
+
+            
+        }
+    }
+}
