177d25396dd8a089cbae8652189bf03a72a0e31f
Ashlyn Wright <362581399@GoTVDSB.ca>
2025-11-03 20:35:19 -0500

    done

--- Program.cs
+++ Program.cs
@@ -7,13 +7,73 @@
             //Ashlyn Wright
             //top7.1 While loop assignment
 
+            //Assignment 1
+
+            //int number1;
+            //int number2;
+            //int number3;
+            //int number4;
+
+            //Console.WriteLine("please enter the first number");
+            //number1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("please enter the second number");
+            //number2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("please enter the third number");
+            //number3 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("please enter the fourth number");
+            //number4 = Convert.ToInt32(Console.ReadLine());
+
+            //int sum = number1 + number2 + number3 + number4;
+            //double average = sum / 4.0;
+
+            //Console.WriteLine("the average of your numbers is " + average + "");
+
+
+
+            //Assignment 2
+
+            Random Random = new Random();
+            int secretNumber = Random.Next(1, 101); 
+            Console.WriteLine("please enter a number between 1 and 100");
+            int userguess = Convert.ToInt32(Console.ReadLine());
+            do
+            {        
+                if (userguess < secretNumber)
+                {
+                    Console.WriteLine("too low, please try again");
+                    userguess = Convert.ToInt32(Console.ReadLine());
+                }
+                else if (userguess > secretNumber)
+                {
+                    Console.WriteLine("too high, please try again");
+                    userguess = Convert.ToInt32(Console.ReadLine());
+                }
+                else 
+                {
+                    Console.WriteLine("Congratulations, you guessed the right number!");
+                    userguess = Convert.ToInt32(Console.ReadLine());
+                }
+            } while (userguess != secretNumber);
+           
+             
 
 
 
 
 
 
-            
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
